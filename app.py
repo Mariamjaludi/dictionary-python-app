b45a4a88d6f9deff3b117663c705dd4cec2e1047
mariamjaludi <mariam.jaludi@gmail.com>
2019-09-04 18:11:34 -0700

    closest word match added

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 # function retrieves words and definitions from json file
@@ -7,7 +8,14 @@ data = json.load(open("data.json"))
 def define(word):
     word = word.lower()
     if word in data:
-        return data[word]
+        return ' '.join(data[word])
+    elif  get_close_matches(word, data.keys()):
+        match = get_close_matches(word, data.keys())[0]
+        reply = input("Did you mean %s? Enter Y if yes, or N if no" %match)
+        if reply == "Y":
+            return ' '.join(data[match])
+        else:
+            return "Word does not exist in dictionary. Please double check it."  
     else: 
         return "Word does not exist in dictionary. Please double check it."    
 
