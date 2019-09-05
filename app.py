f940eab5d56fad74593a880dc0cc6fb6605ae5e9
mariamjaludi <mariam.jaludi@gmail.com>
2019-09-04 17:37:31 -0700

    conditional test added for unknown input

--- app.py
+++ app.py
@@ -3,7 +3,10 @@ import json
 data = json.load(open("data.json"))
 
 def translate(word):
-    return data[word]
+    if word in data:
+        return data[word]
+    else: 
+        return "Word does not exist in dictionary. Please double check it."    
 
 word = input("Enter word: ")
 
